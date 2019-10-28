8f3b7a3bed5889d579614a66a02808923221cf8b
LiZhaji <1157287181@qq.com>
2019-10-28 22:10:25 +0800

    update

--- js/success.js
+++ js/success.js
@@ -17,7 +17,7 @@ function ifSuccess(x, y) {
   } else {
     let fullR = 0;
     piecesCount.forEach(row => {
-      row.findIndex(p => !p) === -1 ? fullR += 1 : '';
+      row.findIndex(p => !p) === -1 ? (fullR += 1) : "";
     });
     if (fullR === piecesCount.length) {
       let msg = "";
@@ -32,16 +32,16 @@ function ifSuccess(x, y) {
 }
 
 // 获胜
-function success(i, j) {
-  const curPiece = piecesCount[i][j]; // 当前位置 0: 空, 1: 黑棋, 2: 白棋
-  const key = "" + curPiece + "_" + i + "_" + j;
+function success(x, y) {
+  const curPiece = piecesCount[x][y]; // 当前位置 0: 空, 1: 黑棋, 2: 白棋
+  const key = "" + curPiece + "_" + x + "_" + y;
   process[key] = process[key] || {};
   const dirs = process[key];
   const tempDir = ["lup", "up", "rup", "l", "r", "lbt", "bt", "rbt"];
 
   // 按照tempDir的顺序 计算该棋子每个方向的相同棋子数
-  for (let jj = j - 1; jj <= j + 1; jj++) {
-    for (let ii = i - 1; ii <= i + 1; ii++) {
+  for (let jj = y - 1; jj <= y + 1; jj++) {
+    for (let ii = x - 1; ii <= x + 1; ii++) {
       if (gameover) {
         console.log("gameover! ");
 
@@ -49,7 +49,7 @@ function success(i, j) {
       }
 
       // 该位置本身不参与记录
-      if (jj === j && ii === i) {
+      if (jj === y && ii === x) {
         continue;
       }
       const curDir = tempDir.shift();
@@ -70,8 +70,8 @@ function success(i, j) {
   return false;
 }
 
-function getDirCount(i, j, curDir, curPiece) {
-  const targetPiece = piecesCount[i][j]; // 该方向上的第一个棋子
+function getDirCount(x, y, curDir, curPiece) {
+  const targetPiece = piecesCount[x][y]; // 该方向上的第一个棋子
   const index = dirs.findIndex(dir => dir === curDir);
   const revDir = dirs[dirs.length - index - 1];
   let tarCount = 1;
@@ -84,19 +84,19 @@ function getDirCount(i, j, curDir, curPiece) {
     const ii = dirOprs[curDir][0];
     const jj = dirOprs[curDir][1];
     const curCount =
-      process["" + targetPiece + "_" + i + "_" + j][curDir] > 0
-        ? process["" + targetPiece + "_" + i + "_" + j][curDir]
+      process["" + targetPiece + "_" + x + "_" + y][curDir] > 0
+        ? process["" + targetPiece + "_" + x + "_" + y][curDir]
         : 1; // 小于0表示没有一样的棋子，不能用该值参与计算
     tarCount = curCount + 1;
 
     // 2. 当前方向上所有的棋子反方向的count 更新
     // 需判断新棋子的反方向有无自家棋子,有则需要全部重新赋值
-    const revCount =
-      process["" + targetPiece + "_" + (i - ii) + "_" + (j - jj)][revDir] > 1 // 大于1表示有
-        ? process["" + targetPiece + "_" + (i - ii) + "_" + (j - jj)][revDir]
-        : 1;
+    const realCount =
+      process["" + targetPiece + "_" + (x - ii) + "_" + (y - jj)][revDir];
+    
+    const revCount = realCount > 1 ? realCount : 1;// 大于1表示有
     for (let k = 0; k < curCount; k++) {
-      process["" + targetPiece + "_" + (i + ii * k) + "_" + (j + jj * k)][
+      process["" + targetPiece + "_" + (x + ii * k) + "_" + (y + jj * k)][
         revDir
       ] += revCount;
     }
@@ -105,7 +105,7 @@ function getDirCount(i, j, curDir, curPiece) {
     if (revCount > 1) {
       for (let k = 2; k <= revCount; k++) {
         let rp = (process[
-          "" + targetPiece + "_" + (i - ii * k) + "_" + (j - jj * k)
+          "" + targetPiece + "_" + (x - ii * k) + "_" + (y - jj * k)
         ][curDir] += tarCount - 1); // 减去新棋的重复
         if (rp >= 5) {
           gameover = true;
@@ -117,7 +117,7 @@ function getDirCount(i, j, curDir, curPiece) {
   } else {
     // 表示第一颗棋为对方棋子, 不仅要返回-2, 还要设置对方棋反方向的count = -2
     tarCount = -2;
-    process["" + targetPiece + "_" + i + "_" + j][revDir] = -2;
+    process["" + targetPiece + "_" + x + "_" + y][revDir] = -2;
     return tarCount;
   }
   if (tarCount >= 5) {
